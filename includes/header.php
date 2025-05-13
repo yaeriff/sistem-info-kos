67210fa9cfc09d72a38c83bd6f7d86ed45117b15
daniabil <ahmaddanii092@gmail.com>
2025-05-13 15:10:49 +0700

    ganti file menjadi php

---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<div class="topbar d-flex align-items-center">
+        <div
+          class="container d-flex justify-content-center justify-content-md-between"
+        >
+          <div class="contact-info d-flex align-items-center">
+            <i class="bi bi-envelope d-flex align-items-center"
+              ><a href="mailto:contact@example.com">contact@example.com</a></i
+            >
+            <i class="bi bi-phone d-flex align-items-center ms-4"
+              ><span>+62 5589 55488 55</span></i
+            >
+          </div>
+          <div class="social-links d-none d-md-flex align-items-center">
+            <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+      </div>
+      <!-- End Top Bar -->
+      <div
+        class="branding d-flex align-items-center"
+        style="background-color: black"
+      >
+        <div
+          class="container position-relative d-flex align-items-center justify-content-between"
+        >
+          <a href="index.html" class="logo d-flex align-items-center">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1 class="sitename">Kos bu Anna</h1>
+            <span>.</span>
+          </a>
+          <nav id="navmenu" class="navmenu">
+            <ul>
+              <li>
+                <a href="#hero" class="active">Beranda<br /></a>
+              </li>
+              <li><a href="#about">Tentang Kos</a></li>
+              <li><a href="#services">Pelayanan</a></li>
+              <li><a href="#portfolio">Fasilitas</a></li>
+              <li><a href="#team">Pembayaran</a></li>
+              <li><a href="#contact">Kontak</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+        </div>
+      </div>
